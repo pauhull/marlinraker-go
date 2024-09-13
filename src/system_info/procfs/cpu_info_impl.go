bed76cb4a95c4e30c3ec47d4f4dddd0459f710e4
phm07 <22707808+phm07@users.noreply.github.com>
2024-09-14 00:16:18 +0200

    fix: do not error if host temp readings not available

--- src/system_info/procfs/cpu_info_impl.go
+++ src/system_info/procfs/cpu_info_impl.go
@@ -2,6 +2,12 @@
 
 package procfs
 
+import (
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
 func GetCpuInfo() (*CpuInfo, error) {
 	return getCpuInfoImpl("/proc/cpuinfo", "/proc/meminfo")
 }
@@ -10,6 +16,17 @@ func GetCpuTimes() (CpuTimes, error) {
 	return getCpuTimesImpl("/proc/stat")
 }
 
+var cpuTempAvail = true
+
 func GetCpuTemp() (float64, error) {
-	return getCpuTempImpl("/sys/class/thermal/thermal_zone0/temp")
+	if !cpuTempAvail {
+		return 0, nil
+	}
+	temp, err := getCpuTempImpl("/sys/class/thermal/thermal_zone0/temp")
+	if err != nil && errors.Is(err, os.ErrNotExist) {
+		log.Warn("Temperature readings not available at /sys/class/thermal/thermal_zone0/temp. Host temperature will not be displayed.")
+		cpuTempAvail = false
+		return 0, nil
+	}
+	return temp, err
 }
