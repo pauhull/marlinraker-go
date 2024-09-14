b53f6d419fe406d230eb36c64b155e309fcd859a
phm07 <22707808+phm07@users.noreply.github.com>
2024-09-14 04:07:35 +0200

    ci: use goreleaser

--- src/constants/constants.go
+++ src/constants/constants.go
@@ -1,6 +1,26 @@
 package constants
 
-const Version = "v" + ApiVersionString + "-alpha"
-const ApiVersionString = "0.0.1"
+import (
+	"strconv"
+	"strings"
+)
 
-var ApiVersion = [3]int{0, 0, 1}
+var (
+	ApiVersionString = "0.0.0"
+	suffix           = "dev"
+
+	Version = func() string {
+		if suffix != "" {
+			return ApiVersionString + "-" + suffix
+		}
+		return ApiVersionString
+	}()
+
+	ApiVersion = func() [3]int {
+		parts := strings.Split(ApiVersionString, ".")
+		major, _ := strconv.Atoi(parts[0])
+		minor, _ := strconv.Atoi(parts[1])
+		patch, _ := strconv.Atoi(parts[2])
+		return [3]int{major, minor, patch}
+	}()
+)
